6b387893d440993f96007038f4f364c2c2df19ea
Kevin Smith <excited_heaps0s@icloud.com>
2019-11-20 18:01:32 -0500

    Update comments to reflect script

--- bitcoin_bpi.sh
+++ bitcoin_bpi.sh
@@ -1,7 +1,5 @@
-# ~/bin/get-volume.sh
+# ~/bin/bitcoin_bpi.sh
 #!/bin/bash
-# Get the maximum volume of any pulseaudio sink channel
-# amixer get Master | egrep -o "[0-9]+%"
 while true; do
   btc=$(curl -s https://api.coindesk.com/v1/bpi/currentprice.json | jq '.bpi.USD.rate')
   echo "BTC: $btc" > $HOME/.sysinfo/bitcoin
