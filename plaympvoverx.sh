5200e801817251e2921d4a7c521a9be221d9344e
Kevin Smith <excited_heaps0s@icloud.com>
2020-11-11 13:54:59 -0500

    Set CFR

--- plaympvoverx.sh
+++ plaympvoverx.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export PULSE_SERVER=192.168.1.110
-ffmpeg -i "$1" -preset veryfast -s 1280x720 -crf 18 -f matroska - | mpv -
+ffmpeg -i "$1" -preset veryfast -s 1280x720 -crf 23 -f matroska - | mpv -
