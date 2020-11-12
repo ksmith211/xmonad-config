42a83720695c5b26fdece58210e52eda72199645
Kevin Smith <excited_heaps0s@icloud.com>
2020-11-12 00:29:45 -0500

    ADd mpv over config to halve aspect ratio

--- plaympvoverx.sh
+++ plaympvoverx.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 export PULSE_SERVER=192.168.1.110
-ffmpeg -i "$1" -preset veryfast -s 1280x720 -crf 18 -f matroska - | mpv -
+WIDTH=$(mediainfo "$1" | grep Width | awk -F':' '{print $2}' | sed -E 's/[a-zA-Z ]*([0-9]+).*/\1/g')
+HEIGHT=$(mediainfo "$1" | grep Height | awk -F':' '{print $2}' | sed -E 's/[a-zA-Z ]*([0-9]+).*/\1/g')
+#ffmpeg -i "$1" -preset veryfast -s 1280x720 -crf 23 -f matroska - | mpv -
+#ffmpeg -i "$1" -preset veryfast -s 960x540 -crf 23 -f matroska - | mpv -
+ffmpeg -i "$1" -preset veryfast -s "$WIDTH"x"$HEIGHT" -crf 18 -f matroska - | mpv -
