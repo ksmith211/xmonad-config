85f37d347c59d8dddb17036190e48d545abb4ad5
Kevin Smith <excited_heaps0s@icloud.com>
2020-11-14 13:53:07 -0500

    Add terminal reset

--- plaympvoverx.sh
+++ plaympvoverx.sh
@@ -5,3 +5,6 @@ export PULSE_SERVER=$(echo $SSH_CLIENT | awk '{print $1}')
 
 # compress to desired format and play
 ffmpeg -i "$1" -preset veryfast -s 960x540 -crf 18 -f matroska - | mpv -
+
+# reset terminal after play
+reset
